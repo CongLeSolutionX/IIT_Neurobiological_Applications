547c1b3343ff872bf39c90bb1bd85ebbf62bd3b7
Cong Le <longchike@gmail.com>
2025-06-30 01:39:52 -0700

    The setup

---
+++ IIT_Neurobiological_Applications/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IIT_Neurobiological_Applications
+//
+//  Created by Cong Le on 6/30/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IIT_Neurobiological_Applications/IIT_Neurobiological_ApplicationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IIT_Neurobiological_ApplicationsApp.swift
+//  IIT_Neurobiological_Applications
+//
+//  Created by Cong Le on 6/30/25.
+//
+
+import SwiftUI
+
+@main
+struct IIT_Neurobiological_ApplicationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
